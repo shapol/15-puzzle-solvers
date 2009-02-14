7edee2dd189dbc6eda9a22ec0d734ae6c8f7d149
tomerpeled <tomerpeled@2b30e70a-fa8d-11dd-984d-ab748d24aa7e>
2009-02-14 15:35:49 +0000

    isDone works

--- AIProject/src/Puzzle/PuzzleGame.java
+++ AIProject/src/Puzzle/PuzzleGame.java
@@ -14,9 +14,9 @@ public class PuzzleGame {
     }
 
     public static enum Algorithm {
+
         AStar, IDAStar
     }
-    
     private PuzzleNode root;
     private final int gameWidth = 15;
     private int nodesSearched = 0; /*The number of nodes we searched so far*/
@@ -25,11 +25,10 @@ public class PuzzleGame {
     This keeps us from moving the same tile back and forth ,and forces the moves taken 
     to advance towrds the goal. Each time we do binary search on it */
     public static Vector<PuzzleNode> prevPos = new Vector<PuzzleNode>();
-    
     /*In our tree of puzzle States this will hold the solution puzzle Node-State*/
     private PuzzleNode currSolutionNode = null;
-    
     private static int[][][] manhatanDistance;
+    private PuzzleNode goalNode;
 
     public PuzzleGame(int[][] puzzle, Point spaceCell) throws Exception {
 
@@ -37,6 +36,13 @@ public class PuzzleGame {
             throw new Exception("InValid Game Width != 15");
         }
         this.root = new PuzzleNode(puzzle, null, spaceCell);
+        int[][] goalPuzzle = {
+            {0, 1, 2, 3},
+            {4, 5, 6, 7},
+            {8, 9, 10, 11},
+            {12, 13, 14, 15}
+        };
+        goalNode = new PuzzleNode(goalPuzzle, null, new java.awt.Point(0, 0));
         initializeManhatanDistance();
     }
 
@@ -67,33 +73,34 @@ public class PuzzleGame {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public boolean isDone(PuzzleNode currNode){
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean isDone(PuzzleNode currNode) {
+        if (currNode.compareTo(goalNode) == 0) {
+            return true;
+        }
+        return false;
     }
-    
-    public void initializeManhatanDistance(){
+
+    public void initializeManhatanDistance() {
         manhatanDistance = new int[gameWidth][4][4];
-        for(int value=0;value<gameWidth;value++){
-            for(int i=0;i<4;i++){
-                for(int j=0;j<4;j++){
-                    manhatanDistance[value][i][j] = Math.abs(value/4 -i)+ Math.abs(value%4-j);
+        for (int value = 0; value < gameWidth; value++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    manhatanDistance[value][i][j] = Math.abs(value / 4 - i) + Math.abs(value % 4 - j);
                 }
             }
-        }       
+        }
     }
-    
-    public static int getManahtanDistance(PuzzleNode puzzleNode){
+
+    public static int getManahtanDistance(PuzzleNode puzzleNode) {
         int distance = 0;
         int[][] puzzle = puzzleNode.getPuzzle();
         int[] puzzleRaw;
-        for(int i=0;i<puzzle.length;i++){
+        for (int i = 0; i < puzzle.length; i++) {
             puzzleRaw = puzzle[i];
-            for (int j=0;j<puzzleRaw.length;j++){
-                distance += manhatanDistance[puzzle[i][j]-1][i][j];
+            for (int j = 0; j < puzzleRaw.length; j++) {
+                distance += manhatanDistance[puzzle[i][j] - 1][i][j];
             }
-        }        
-        return distance;        
+        }
+        return distance;
     }
-
-            
 }
